92646d78c2af84a66a29298e9fa4e3f1071664e0
Grzegorz Sowinski <88460670+hotchkiss88@users.noreply.github.com>
2021-09-14 18:32:14 +0000

    re - work

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-let table = [12, 67, 34, 23.01, 24, 2, 56, 8, 0x10, 23.02, 78, 34, 1e2, 45, 67, 98, 1]
-
 function sorting(x) {
-    let storageSmallest = 0;
-    for (i = 1; i < x.length; i++) {
+    let storage = 0;
+    for (i = 0; i < x.length; i++) {
 
-        storageSmallest = x[i];
+        let storageSmallest = i;
 
-        let j = i - 1;
-
-        while (j >= 0 && x[j] > storageSmallest) {
-            x[j + 1] = x[j];
-            j = j - 1;
+        for (let j = i + 1; j < x.length; j++) {
+            if (x[j] < x[storageSmallest]) {
+                storageSmallest = j;
+            }
         }
-        x[j + 1] = storageSmallest;
-
+        if (storageSmallest !== i) {
+            storage = x[storageSmallest];
+            x[storageSmallest] = x[i];
+            x[i] = storage;
+        }
     }
-    return storageSmallest;
+    return storage;
 }
 
 sorting(table);
