62418fa3544e4d9951f3b7d41987b6af8304aa4d
Grzegorz Sowinski <88460670+hotchkiss88@users.noreply.github.com>
2021-09-15 12:15:52 +0000

    format

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 let table = [12, 67, 34, 23.01, 24, 2, 56, 8, 0x10, 23.02, 78, 34, 1e2, 45, 67, 98, 1]
 
 function sorting(x) {
-    let storage = 0;
-    for (i = 0; i < x.length; i++) {
+  let storage = 0;//przechowuje najmniejszą wartość
+  for (i = 0; i < x.length; i++) {
 
-        let storageSmallest = i;
+    let storageSmallest = i;//przechowuje index najmniejszej wartości
 
-        for (let j = i + 1; j < x.length; j++) {
-            if (x[j] < x[storageSmallest]) {
-                storageSmallest = j;
-            }
-        }
-        if (storageSmallest !== i) {
-            storage = x[storageSmallest];
-            x[storageSmallest] = x[i];
-            x[i] = storage;
-        }
+    for (let j = i + 1; j < x.length; j++) {
+      if (x[j] < x[storageSmallest]) {
+        storageSmallest = j;//sortuje tablicę
+      }
     }
-    return storage;
+    if (storageSmallest !== i) {
+      storage = x[storageSmallest];//Przypisuje najmniejszy element do tablicy
+      x[storageSmallest] = x[i];
+      x[i] = storage;
+    }
+  }
+  return storage;
 }
 
 sorting(table);
